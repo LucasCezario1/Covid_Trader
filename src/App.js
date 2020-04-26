074c06511d166b7c0bf23ceb64199a50de31ab8c
Quenobe <lucasdsc96@gmail.com>
2020-04-26 19:31:43 -0300

    Covid-19

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { Cards, Chart, CountryPicker} from './Components';
+import styles from './App.module.css';
+import {fetchData } from './api';
+
+import coronaImage from './images/image.png';
+
+class App extends React.Component{
+  state ={
+      data: {},
+      country: '',
+  }
+  async componentDidMount(){
+    const fetchDatadata = await fetchData();
+    this.setState({data: fetchDatadata})
+  }
+
+  handleCountryChange = async(country) =>{
+    const fetchedData = await fetchData(country);
+    
+    this.setState({data:fetchedData, country:country});
+
+  }
+
+  render(){
+    const {data , country} = this.state;
+
+    return(
+        <div className={styles.container}>
+       <img className={styles.image} src={coronaImage} alt="COVID-19"/> 
+       <Cards data={data}/>
+       <CountryPicker handleCountryChange={this.handleCountryChange}/>
+       <Chart data={data} country={country}/>
+      </div>
+    )
+  }
+}
+
+export default App;
